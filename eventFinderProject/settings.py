b1b2a405cc84f1853da7601a9eb48db78ef800d0
Eleanor <Eleanor.lober@gmail.com>
2019-09-07 12:17:27 +0800

    .gitignore

--- eventFinderProject/settings.py
+++ eventFinderProject/settings.py
@@ -77,9 +77,9 @@ WSGI_APPLICATION = 'eventFinderProject.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'freeeventfinder', # you will need to create this db
-        'USER': '', # enter your user name here
-        'PASSWORD': '',
+        'NAME': 'database_main', # you will need to create this db
+        'USER': 'postgres', # enter your user name here
+        'PASSWORD': 'postgres',
         'HOST': 'localhost',
         'PORT': '5432',
     }
---
+++ myenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\eleanor\desktop\shecod~1\fun_ti~1\fun-ti~1\myenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
